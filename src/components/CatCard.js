4d86fd6eeea896b505241fb46783b83c8e29878e
antskat <donaldsmak7@gmail.com>
2023-12-28 16:34:00 +0200

    add swiper

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -2,7 +2,7 @@ const CatCard = ({ data }) => {
     return ( 
         <li className="swiper-list-item">
             <img src={data.img} className="swiper-list-img" alt="cat" />
-            <h3 className="swiper-list-text">{data.name}</h3>
+            <h3 className="swiper-list-title">{data.name}</h3>
         </li>
      );
 }
--- src/pages/AboutCats.js
+++ src/pages/AboutCats.js
@@ -4,25 +4,52 @@ import { Navigation, Pagination } from "swiper/modules";
 import "./styles/swiper.css";
 import "../styles/about-cats.css";
 import CatCard from "../components/CatCard";
+import BritishShorthairCat from "../img/british-shortcair-cat.png";
+import Sphynx from "../img/sphynx.png";
+import SiameseCat from "../img/siamese-cat.png";
+import AbyssinianCat from "../img/abyssinian-cat.png";
+import MunchkinCat from "../img/munchkin-cat.png";
+import BurmeseCat from "../img/burmese-cat.png";
+import AmericanCurlCat from "../img/american-curl-cat.png";
+import ExoticShortcairCat from "../img/exotic-shortcair-cat.png";
+import TabbyCat from "../img/tabby-cat.png";
 
 const AboutCats = () => {
   const catData = [
-    { id: 1, name: "British Shorthair Cat", img: "" },
-    { id: 2, name: "Sphynx", img: "" },
-    { id: 3, name: "Siamese Сat", img: "" },
-    { id: 4, name: "Abyssinian Сat", img: "" },
-    { id: 5, name: "Munchkin Сat", img: "" },
-    { id: 6, name: "Burmese Сat", img: "" },
-    { id: 7, name: "American Curl", img: "" },
-    { id: 8, name: "Exotic Shorthair", img: "" },
-    { id: 9, name: "Tabby Cat", img: "" },
+    { id: 1, name: "British Shorthair Cat", img: BritishShorthairCat },
+    { id: 2, name: "Sphynx", img: Sphynx },
+    { id: 3, name: "Siamese Сat", img: SiameseCat },
+    { id: 4, name: "Abyssinian Сat", img: AbyssinianCat },
+    { id: 5, name: "Munchkin Сat", img: MunchkinCat },
+    { id: 6, name: "Burmese Сat", img: BurmeseCat },
+    { id: 7, name: "American Curl", img: AmericanCurlCat },
+    { id: 8, name: "Exotic Shorthair", img: ExoticShortcairCat },
+    { id: 9, name: "Tabby Cat", img: TabbyCat },
+    { id: 10, name: "Soamli cat", img: "" },
+    { id: 11, name: "Ragdoll", img: "" },
+    { id: 12, name: "Cornish Rex", img: "" },
+    { id: 13, name: "Maine Coon", img: "" },
+    { id: 14, name: "Persian Сat", img: "" },
+    { id: 15, name: "Charteux", img: "" },
+    { id: 16, name: "Bombany Cat", img: "" },
+    { id: 17, name: "Bengal Cat", img: "" },
+    { id: 18, name: "Oriental Cat", img: "" },
+    { id: 19, name: "Norwegian Forest Cat", img: "" },
+    { id: 20, name: "Domestic Shortcair Cat", img: "" },
+    { id: 21, name: "Birman Сat", img: "" },
+    { id: 22, name: "Australian Mist", img: "" },
+    { id: 23, name: "Scottish Fold Сat", img: "" },
+    { id: 24, name: "Burmese Сat", img: "" },
+    { id: 25, name: "American Curl", img: "" },
+    { id: 26, name: "Exotic Shorthair", img: "" },
+    { id: 27, name: "Tabby Cat", img: "" },
   ];
 
   // Разделяем catData на три подмассива
   const slidesData = [
-    catData.slice(0, 3),
-    catData.slice(3, 6),
-    catData.slice(6, 9),
+    catData.slice(0, 9),
+    catData.slice(9, 18),
+    catData.slice(18, 27),
   ];
 
   return (
