182ee2b553240d63f3ed93e87ac416986129465e
antskat <donaldsmak7@gmail.com>
2023-12-30 18:51:16 +0200

    Update CatopiaRedirect.js

--- src/utils/CatopiaRedirect.js
+++ src/utils/CatopiaRedirect.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 const CatopiaRedirect = () => {
-    return <Navigate to="/catopia/auth/me" />;
+    return <Navigate to="/auth/me" />;
 }
  
 export default CatopiaRedirect;
