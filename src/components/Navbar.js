cb4b05c3dfc867d8717987a4bc6b76e6200226d2
antskat <donaldsmak7@gmail.com>
2024-01-04 20:43:55 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const UserProfile = () => {
         <Link to="/profile">
           <img
             style={{ borderRadius: "50%", width: "40px", height: "40px" }}
-            src={`https://catopia-backendd.onrender.com/avatars/${avatar}`}
+            src={`https://catopia-backendd.onrender.com/uploads/${avatar}`}
             alt="User Avatar"
           />
         </Link>
