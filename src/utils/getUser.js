b3eb3b809cd0e69716103e7d4b516054d1381335
antskat <donaldsmak7@gmail.com>
2024-01-06 22:27:27 +0200

    Update getUser.js

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -9,7 +9,7 @@ const AllUserProfile = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:8888/getUser", {
+        const response = await axios.get("https://catopia-backendd.onrender.com/getUser", {
           headers: {
             Authorization: `Bearer ${authToken}`,
           },
